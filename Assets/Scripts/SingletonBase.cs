704f91a77fcfa03bcc3244bd5299a9adaf8d3c65
Vinay Kashyap <vinay@outscal.com>
2019-01-29 18:25:06 +0530

    MultipleBullet Features

---
+++ Assets/Scripts/SingletonBase.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+
+public class SingletonBase<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+                DontDestroyOnLoad(instance);
+            }
+            else
+            {
+               // Debug.Log("null hai");
+               // Destroy(instance);
+            }
+            return instance;
+        }
+    }
+}
