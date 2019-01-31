533f16732ddc76c6dbb126d24fd51f63517585ae
Vinay Kashyap <vinay@outscal.com>
2019-01-31 12:52:57 +0530

    Input component added

--- Assets/Scripts/Common/InputManagerBase.cs
+++ Assets/Scripts/Common/InputManagerBase.cs
@@ -1,13 +1,33 @@
-﻿using System;
+﻿using InputComponents;
+using Player;
+using Enemy;
+using UnityEngine;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Common
 {
     public class InputManagerBase : SingletonBase<InputManagerBase>
     {
+        [SerializeField]
+        private List<PlayerController> listOfPlayerControllers = new List<PlayerController>();
+        [SerializeField]
+        private List<EnemyController> listOfEnemyControllers = new List<EnemyController>();
 
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+           
+        }
 
+        public void PopulatePlayerList(PlayerController _playerController)
+        {
+           listOfPlayerControllers.Add(_playerController);
+        }
+        
+        private void Update()
+        {          
+            
+        }
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,28 +1,35 @@
 ﻿using UnityEngine;
 using Weapons.Bullet;
+using InputComponents;
 
 namespace Player
 {
     public class PlayerController
     {
-        PlayerView playerView;
-        PlayerModel playerModel;
+        private PlayerView playerView;
+        private PlayerModel playerModel;
+        private InputComponent currentInputComponent;
 
         public PlayerController(PlayerView playerViewInstance)
         {
+            playerModel = new PlayerModel();            
+            playerView = playerViewInstance;
+            currentInputComponent = new InputComponent();
+        }
+        public PlayerController(PlayerView playerViewInstance, InputScriptabelObject _customInputScheme)
+        {
             playerModel = new PlayerModel();
             playerView = playerViewInstance;
+            currentInputComponent = new InputComponent(_customInputScheme);
         }
         public void Move(float h, float v)
         {
             playerView.MovePlayer(h, v, playerModel.GetSpeed());
         }
-
         public void DisplayPlayerStats()
         {
             Debug.Log("ID: " + playerModel.GetID().ToString() + "Player name:" + playerModel.GetName() + "Player Speed:" + playerModel.GetSpeed().ToString());
         }
-
         public void Fire()
         {
             var _bulletController = BulletService.Instance.SpawnBullet();
@@ -32,7 +39,6 @@ namespace Player
 
             playerView.OnFirePressed(_bulletController, _bulletSpeed);
         }
-
         public void RotatePlayer(float pitch)
         {
             playerView.RotatePlayer(pitch);
