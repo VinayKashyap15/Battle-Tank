d9e36f9443e7e376f61af329feca6488c49d447b
Vinay Kashyap <vinay@outscal.com>
2019-02-01 17:07:09 +0530

    meta files

---
+++ Assets/Scripts/SceneSpecific/SceneScriptableObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SceneSpecific
+{
+    [CreateAssetMenu(fileName = "NewGameScene", menuName = "Custom Objects/Scene", order = 0)]
+    public class SceneScriptableObject : ScriptableObject
+    {
+        public Object gameScene;
+        
+        
+    }
+}
