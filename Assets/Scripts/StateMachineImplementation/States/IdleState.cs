ccab4ac4de5f097eaa6b7681fdc10a5ebe6642b1
Vinay Kashyap <vinay@outscal.com>
2019-02-07 17:06:28 +0530

    idle animation test

--- Assets/Scripts/StateMachineImplementation/States/IdleState.cs
+++ Assets/Scripts/StateMachineImplementation/States/IdleState.cs
@@ -18,15 +18,12 @@ namespace PlayerStates
            _currentPlayerAnimator = _playerViewInstance.GetAnimator();
         }
         public override void OnStateExit()
-        {
-            Debug.Log("Exitting idle state of player");
-            //disable idle animation
+        {        
             _currentPlayerAnimator.SetBool("isIdle",false);
 
         }
         public override void OnStateUpdate()
-        {
-            
+        {            
             _currentPlayerAnimator.SetBool("isIdle",true);
         }
     }
